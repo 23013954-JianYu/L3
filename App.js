874e6bd77765ca315dc0ad6adf8ff285249075f0
23013954-JianYu <23013954@myrp.edu.sg>
2024-11-12 15:02:47 +0800

    tt

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, TextInput, Button, Alert, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Custom Component
+const InputBox = ({label, onChangeText})=> {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText}/>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MyApp = () => {
+    // let username = "";
+    // let pw = "";
+    const [pw, setPw] = useState('');
+    const [username, setUsername] = useState('');
+    const [userType, setUserType] = useState('');
+
+  return (
+      <View style={{padding: 20, paddingTop: 50}}>
+          {/*EXERCISE 1B*/}
+          <Text>User Type:</Text>
+          <RNPickerSelect
+              onValueChange={(value) => setUserType(value)}
+              items={[
+                  {label: 'Admin', value: 'Admin'},
+                  {label: 'Guest', value: 'Guest'}
+              ]}
+          />
+
+          {/*STATE*/}
+          {/*<Text>Password:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onChangeText={(text) => setPw(text)}*/}
+          {/*/>*/}
+
+          {/*<TouchableOpacity onPress={()=>ToastAndroid.show(pw, ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+          {/*<Text>{pw}</Text>*/}
+
+
+          {/*Getting Text Input Values*/}
+          {/*<Text>Username:</Text>*/}
+          {/*<TextInput style={{borderWidth:1}}*/}
+          {/*           onChangeText={(text) => username = text} />*/}
+          {/*<Text>Password:</Text>*/}
+          {/*<TextInput style={{borderWidth:1}}*/}
+          {/*           onChangeText={(text) => pw = text} />*/}
+          {/*<TouchableOpacity onPress={() => ToastAndroid.show(pw, ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*EXERCISE 1A*/}
+          {/*<Text>Password:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}} />*/}
+
+          {/*EXERCISE 1C & 1D*/}
+          {/*<Button*/}
+          {/*    title="LOG IN"*/}
+          {/*    onPress={() => Alert.alert("Welcome!")}*/}
+          {/*/>*/}
+
+          {/*EXERCISE 1E*/}
+          {/*<TouchableOpacity*/}
+          {/*    onPress={() => Alert.alert("2nd Welcome!")}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*EXERCISE 1F*/}
+          {/*<TouchableOpacity*/}
+          {/*    onPress={() => ToastAndroid.show("Welcome!",ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN 2</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          {/*EXERCISE 1G*/}
+
+          {/*<Text>User Name:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onChangeText={(text) => setUsername(text)}*/}
+          {/*/>*/}
+
+          {/*<Text>Password:</Text>*/}
+          {/*<TextInput style={{borderWidth: 1}}*/}
+          {/*           onChangeText={(text) => setPw(text)}*/}
+          {/*/>*/}
+
+          <InputBox label="User Name:" onChangeText={(text) => setUsername(text)}/>
+          <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+
+          {/*<TouchableOpacity*/}
+          {/*    onPress={() => ToastAndroid.show("Welcome " + userType + " " + username, ToastAndroid.SHORT)}>*/}
+          {/*    <Text>LOG IN</Text>*/}
+          {/*</TouchableOpacity>*/}
+
+          <TouchableOpacity onPress={()=> {
+              const correctPassword = '123';
+              let mymessage = 'Error! Wrong Password!';
+              if (pw == correctPassword) {
+                  mymessage = 'Welcome ' + userType + ' ' + username;
+              }
+              ToastAndroid.show(mymessage, ToastAndroid.SHORT);
+          }
+          }>
+              <Text>LOG IN</Text>
+          </TouchableOpacity>
+
+      </View>
+  );
+};
+
+export default MyApp;
